3b26cd0aa857edbf885814352ee752d32a36ebdc
tiankaix <104737400+tiankaix@users.noreply.github.com>
2022-05-09 14:41:02 -0400

    Update UserAccount.java

--- UserAccount.java
+++ UserAccount.java
@@ -1,11 +1,24 @@
+import java.util.ArrayList;
+
 public class UserAccount extends Account{
   // data fields
-  private Playlist[] playlists;
-  
+  private ArrayList<Playlist> playlists;
+
   //constructors
-  
+  //add constructor that takes in string username and loads info from database
+  public UserAccount(String s)
+  {
+  }
+
+  //methods
+  public void addPlaylist(String name) //FINISH calls playlist constructor, adds to playlist arraylist
+  {
+
+  }
+
   //getters
-  public Playlist[] getPlaylists(){
-  return this.playlists;
+  public Playlist[] getPlaylists()
+  {
+    return (Playlist[]) playlists.toArray();
   }
 }
