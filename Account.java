b3b41c0f643703d80f33fa1a54e7a14dd79f8fce
tiankaix <104737400+tiankaix@users.noreply.github.com>
2022-05-09 14:41:53 -0400

    Update and rename account.java to Account.java

--- Account.java
+++ Account.java
@@ -1,49 +1,49 @@
-public class account
+public class Account
 {
 	//STATE:
 	private String username;
 	private String password;
-	
+
 	//CONSTRUCTORS:
-	public account() 
+	public Account()
 	{
 		username = "";
 		password = "";
 	}
-	
-	public account(String u, String p){
+
+	public Account(String u, String p){
 		username = u;
 		password = p;
 	}
-	
+
 	//OTHER METHODS:
-	public void deleteAccount() 
+	public void deleteAccount()
 	{
 		username = "";
 		password = "";
 	}
-	
+
 	//SETTERS:
-	public void setUsername(String u) 
+	public void setUsername(String u)
 	{
 		username = u;
 	}
-	
-	public void setPassword(String p) 
+
+	public void setPassword(String p)
 	{
 		password = p;
 	}
-	
+
 	//GETTERS:
 
-	public String getUsername() 
+	public String getUsername()
 	{
 		return username;
 	}
-	
-	public String getPassword() 
+
+	public String getPassword()
 	{
 		return password;
 	}
-	
+
 }
